66bbba045e92bd45a08626968a94f477f1af12e1
najib.tahar-berrabah <najibtaharberrabah@gmail.com>
2020-02-11 11:13:35 +0100

    t

--- src/DataFixtures/GreenSpaceFixtures.php
+++ src/DataFixtures/GreenSpaceFixtures.php
@@ -4,20 +4,31 @@ namespace App\DataFixtures;
 
 use App\Entity\GreenSpace;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class GreenSpaceFixtures extends Fixture
+class GreenSpaceFixtures extends Fixture implements OrderedFixtureInterface
 {
-    public function load(ObjectManager $manager)
+	public function load(ObjectManager $manager)
+	{
+		$file = file_get_contents(__DIR__ . '/../Data/espaces_verts.json');
+		$green_space_json = json_decode($file, true);
+		// $green_space_array = [];
+		// foreach ($green_space_json as $green_space) {
+		// 	array_push($green_space_array, $green_space['fields']['adresse_codepostal']);
+		// }
+		// $green_space_total = array_count_values($green_space_array);
+		// $green_space_district = 
+		foreach ($green_space_json as $green_space) {
+			$greenSpace = (new GreenSpace())
+				->setDistrict($green_space['fields']['adresse_codepostal']);
+			$manager->persist($greenSpace);
+		}
+		$manager->flush();
+	}
+
+	public function getOrder()
     {
-        $file = file_get_contents(__DIR__.'/../Data/espaces_verts.json');
-        $green_space_array = json_decode($file, true);
-        foreach($green_space_array as $green_space) {
-            $greenSpace = (new GreenSpace())
-                           ->setDistrict($green_space['fields']['adresse_codepostal'])
-            ;
-            $manager->persist($greenSpace);
-          }
-        $manager->flush();
+        return 1; // the order in which fixtures will be loaded
     }
 }
---
+++ src/Entity/District.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DistrictRepository")
+ */
+class District
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $green_space;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $size;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGreenSpace(): ?string
+    {
+        return $this->green_space;
+    }
+
+    public function setGreenSpace(string $green_space): self
+    {
+        $this->green_space = $green_space;
+
+        return $this;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(int $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+}
